0b37990835f7e2c67e35fce104784e89d6de937c
Krishna Chaitanya <ktangirala@moldeanalytics.com>
2019-10-18 14:48:08 +0530

    end page changes

--- end.js
+++ end.js
@@ -11,14 +11,14 @@ username.addEventListener('keyup', () => {
 });
 document.getElementById('saveHighScore').addEventListener('click', (eve) => {
     eve.preventDefault()
-    alert('inside')
-
-
     const score = {
         score: currentScore,
         name: username.value
     }
-
     highScores.push(score);
+    highScores.sort((a,b)=>{
+        return b.score - a.score;
+    })
+    highScores.splice(5);
     localStorage.setItem('highscores', JSON.stringify(highScores));
 })
